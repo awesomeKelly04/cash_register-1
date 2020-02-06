ae0689cc081cea164abfcd6e3adbdcf71aa0f114
Joshua Christian <joshua3christianx@gmail.com>
2020-02-06 12:21:13 +0100

    update on data structure and management

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from time import ctime
 
+
+def save_data(client_name, data):
+    
+    DATA.append(
+        {
+            'customer_name': client_name,
+            'product': data,
+            'time': ctime()
+        }
+    )
+
+
+def end_program(client_name, data=None):
+    if data is not None:
+        save_data(client_name, data)
+
+    print(DATA)
+    exit()
+
 def get_number_input(prompt):
     '''
     use prompt to collects input and return float
@@ -15,7 +34,7 @@ def get_number_input(prompt):
             return value
 
         except KeyboardInterrupt:
-            exit()
+            end_program()
 
         except ValueError as e:
             print("Invalid Input!")
@@ -24,21 +43,48 @@ def get_number_input(prompt):
 def main():
     while True:
         client_name  = input('What is the customer\'s name?: ')
-
         if not client_name:
-            break
+            print('Name must be provided!')
+            continue
+
+        elif client_name == '-1':
+            end_program()
+        
+        # store all product details
+        product_information = []
 
         while True:
             product_name = input('what is the product name?: ')
+            
             if not product_name:
-                break
+                print('Product name must be provided!')
+                continue
 
+            elif product_name == '-1':
+                end_program(client_name, data=product_information)
+            
             product_qty = get_number_input(f'How many {product_name} purchased?: ')
+            if product_qty == -1:
+                end_program(client_name, data=product_information)
+
             product_price = get_number_input(f"How much is {product_name}?: ")
+            if product_price == -1:
+                end_program(client_name, data=product_information)
+
+            product_information.append(
+                {
+                    'product_name':product_name,
+                    'product_quantity':product_qty,
+                    'product_price':product_price
+                }
+            )
+
+        # save_data()
 
 if __name__ == '__main__':
     # superglobals
     ERROR_FILE = open('error_log.txt', 'a')
+    DATA = []
 
     # the main code
     main()

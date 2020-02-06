6dca6770443d543516bf168289a10c27e8d3c4dd
Joshua Christian <joshua3christianx@gmail.com>
2020-02-06 16:14:34 +0100

    show logs and input collection achieved

--- main.py
+++ main.py
@@ -1,23 +1,52 @@
-from time import ctime
+from time import strftime, ctime
 
+def show_logs():
+    print("{:^12s} | {:^12s} | {:^4s} | {:^9s} | {:^15s} | {:^16s}"
+          .format("Client", "Product", "Qty", "Price", "Total", "Time"))
+    print("-" * 84)
 
-def save_data(client_name, data):
-    
+    for transaction in DATA:
+        client_name = transaction['customer_name']
+        products = transaction['product']
+        time = transaction['time']
+
+        overall_total = 0
+        for product in products:
+            product_name = product['product_name']
+            product_quantity = product['product_quantity']
+            product_price = product['product_price']
+
+            total = product_price * product_quantity
+            overall_total += total
+
+            total = f'{total:,.2f}'
+            product_price = f'{product_price:,.2f}'
+            print(f"{client_name:<12s} | {product_name:<12s} | {product_quantity:^4.0f} | {product_price:>9s} | {total:>15s} | {time:>16s}")
+            client_name = ''
+
+        overall_total = f'{overall_total:,.2f}'
+        print("{:^12s} | {:^12s} | {:^4s} | {:^9s} | {:>15s} | {:>16s}"
+              .format("", "", "", "", overall_total, time))
+        print("-" * 84)
+
+def save_data(client_name, product_information):
+
     DATA.append(
         {
             'customer_name': client_name,
-            'product': data,
-            'time': ctime()
+            'product': product_information,
+            'time': strftime("%d/%m/%Y %H:%M")
         }
     )
 
 
-def end_program(client_name, data=None):
-    if data is not None:
-        save_data(client_name, data)
+def end_program(client_name=None, product_information=None):
+    if product_information is not None:
+        save_data(client_name, product_information)
 
-    print(DATA)
+    show_logs()
     exit()
+
 
 def get_number_input(prompt):
     '''
@@ -40,46 +69,52 @@ def get_number_input(prompt):
             print("Invalid Input!")
             print(ctime(), e, file=ERROR_FILE)
 
+
 def main():
     while True:
-        client_name  = input('What is the customer\'s name?: ')
+        client_name = input('What is the customer\'s name?: ')
         if not client_name:
             print('Name must be provided!')
             continue
 
         elif client_name == '-1':
             end_program()
-        
+
         # store all product details
         product_information = []
 
         while True:
             product_name = input('what is the product name?: ')
-            
+
             if not product_name:
                 print('Product name must be provided!')
                 continue
 
             elif product_name == '-1':
-                end_program(client_name, data=product_information)
-            
-            product_qty = get_number_input(f'How many {product_name} purchased?: ')
+                # end_program(client_name, product_information)
+                save_data(client_name, product_information)
+                break
+
+            product_qty = get_number_input(
+                f'How many {product_name} purchased?: ')
             if product_qty == -1:
-                end_program(client_name, data=product_information)
+                save_data(client_name, product_information)
+                break
 
             product_price = get_number_input(f"How much is {product_name}?: ")
             if product_price == -1:
-                end_program(client_name, data=product_information)
+                # end_program(client_name, product_information)
+                save_data(client_name, product_information)
+                break
 
             product_information.append(
                 {
-                    'product_name':product_name,
-                    'product_quantity':product_qty,
-                    'product_price':product_price
+                    'product_name': product_name,
+                    'product_quantity': product_qty,
+                    'product_price': product_price
                 }
             )
 
-        # save_data()
 
 if __name__ == '__main__':
     # superglobals
